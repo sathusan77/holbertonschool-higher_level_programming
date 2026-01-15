cd6cc79be9fa7dcbdd65f6ab5aa9ce56654d6def
jeyakumar sathusan <jeyakumar.sathusan@gmail.com>
2026-01-15 15:28:36 +0000

    o

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+import sys
+sys.path.insert(0, '/tmp/')
 import hidden_4
 
 if __name__ == "__main__":
     for name in sorted(dir(hidden_4)):
         if not name.startswith("__"):
             print(name)
+
