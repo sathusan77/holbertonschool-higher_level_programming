42f15a5cb6ab2463de7aa4a237181769a64a5902
jeyakumar sathusan <jeyakumar.sathusan@gmail.com>
2026-01-23 00:52:36 +0100

    o

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    """Print at most x elements of a list and return the number printed."""
+    count = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            count += 1
+        except IndexError:
+            break
+    print()
+    return count
